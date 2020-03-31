657f81d678cbcc1e850b35fcd36ebb58528147e9
Arseny Kruglikov <Kruglikov.A.D@sberbank.ru>
2020-03-31 14:25:05 +0300

    exctract store logic from AddEditUser

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,110 @@
-import React, { useState, FC} from "react";
-// import logo from './logo.svg';
+import React, { FC, useCallback } from "react";
 import "./App.css";
-import users from './stores/menu-store';
-import {User} from './stores/menu-store';
-import {useLocalStore, observer} from 'mobx-react-lite';
-// import { userInfo } from 'os';
-// import { stringify } from 'querystring';
+import users from "./stores/menu-store";
+import { User } from "./stores/menu-store";
+import { observer, useAsObservableSource, Observer } from "mobx-react-lite";
 
+const AddEditUser: FC<Partial<User> & { isEdit?: boolean; saveUser(user: User): void }> = ({
+  isEdit,
+  saveUser,
+  children,
+  ...props
+}) => {
+  const localStore = useAsObservableSource<User>({
+    age: props.age || 0,
+    id: props.id || Math.random(),
+    name: props.name || "",
+    surname: props.surname || ""
+  });
 
-const AddEditUser: FC<{edit: boolean}> = 
-({edit}) => {
-  const userStore = useLocalStore(() => ({
-    currentAddUser: {id: Math.random(), name: '', surname: '', age: 0},
-    get currentUser() {
-      return edit ? editUser : userStore.currentAddUser
+  const inputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = event.target;
+      if (name === "name") {
+        localStore.name = value;
+      } else if (name === "surname") {
+        localStore.surname = value;
+      } else if (name === "age") {
+        localStore.age = Number(value);
+      }
     },
-  }))
-  const [user, setUser] = useState(userStore.currentUser);
+    [localStore.age, localStore.name, localStore.surname]
+  );
 
-  function inputChange(e: any) {
-    const {name, value} = e.target;
-    setUser({...user, [name]: value})
-  }
-  function handleInput() {
-    edit ? users.editUser(user) : users.addUser(user);
-    setUser({id: Math.random(), name: '', surname: '', age: 0});
-  }
-  return(
+  const handleClick = useCallback(() => {
+    saveUser({ ...localStore });
+    localStore.name = "";
+    localStore.age = 0;
+    localStore.surname = "";
+    localStore.id = Math.random();
+  }, [localStore, saveUser]);
+
+  return (
     <>
-      Name:<input value={user.name} name="name" onChange={inputChange}/>
-      Surname:<input value={user.surname} name="surname" onChange={inputChange}/>
-      Age:<input value={user.age} name="age" onChange={inputChange}/>
-      <button onClick={handleInput}>{edit ? 'Edit User' : 'AddUser'}</button>
+      Name:
+      <Observer>{() => <input value={localStore.name} name="name" onChange={inputChange} />}</Observer>
+      Surname:
+      <Observer>{() => <input value={localStore.surname} name="surname" onChange={inputChange} />}</Observer>
+      Age:
+      <Observer>{() => <input value={localStore.age} name="age" onChange={inputChange} />}</Observer>
+      <Observer>{() => <button onClick={handleClick}>{isEdit ? "Edit User" : "AddUser"}</button>}</Observer>
     </>
-  )
-}
+  );
+};
 
-let editUser: User = {id: Math.random(), name: '', surname: '', age: 0}; 
-
-const ViewUsers: FC<{user: User}> = ({user}) => {
-  return (<>
-    <div className="row">
-    <div className="col">{user.id}</div>
-    <div className="col">{user.name}</div>
-    <div className="col">{user.surname}</div>
-    <div className="col">{user.age}</div>
-    <button onClick={()=>users.deleteUser(user.id)}>Delete</button>
-    <button onClick={()=>{users.edit = true; editUser = user} }>Edit</button>
-  </div>
-  </>)
-}
+const ViewUsers: FC<{ user: User; setUserToEdit(user: User): void }> = ({ user, setUserToEdit }) => {
+  return (
+    <>
+      <div className="row">
+        <div className="col">{user.id}</div>
+        <div className="col">{user.name}</div>
+        <div className="col">{user.surname}</div>
+        <div className="col">{user.age}</div>
+        <button onClick={() => users.deleteUser(user.id)}>Delete</button>
+        <button
+          onClick={() => {
+            setUserToEdit(user);
+          }}
+        >
+          Edit
+        </button>
+      </div>
+    </>
+  );
+};
 
 const App = observer(() => {
+  const handleEdit = useCallback((user: User) => {
+    users.setUserToEdit(user);
+  }, []);
+
   return (
     <>
       <div>
         <h2>AddUser</h2>
-        <AddEditUser edit={false}/>
+        <AddEditUser saveUser={user => users.addUser(user)} />
       </div>
-      {users.edit ? (<div>
-        <h2>Edit user</h2>
-        <AddEditUser edit={true}/>
-      </div>) : <></>}
+      {users.userToEdit ? (
+        <div>
+          <h2>Edit user</h2>
+          <AddEditUser
+            isEdit
+            saveUser={user => users.editUser(user)}
+            name={users.userToEdit.name}
+            surname={users.userToEdit.surname}
+            age={users.userToEdit.age}
+            id={users.userToEdit.id}
+          />
+        </div>
+      ) : null}
       <div>
         <h2>View Users</h2>
-        {users.arr.map(user => <ViewUsers user={user}/>)}
+        {users.arr.map(user => (
+          <ViewUsers setUserToEdit={handleEdit} user={user} />
+        ))}
       </div>
-    </>)
-})
+    </>
+  );
+});
 
 export default App;
--- src/stores/menu-store.tsx
+++ src/stores/menu-store.tsx
@@ -2,33 +2,41 @@
 import { setServers } from "dns";
 // import { userInfo } from 'os';
 // import { stringify } from 'querystring';
-import {observable, computed} from 'mobx';  
+import { observable, computed } from "mobx";
 
 export interface User {
-    id: number;
-    name: string;
-    surname: string;
-    age: number;
-  }
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
+}
 class Users {
-    @observable arr = [ 
-            {id: 1, name: 'Tanya', surname: 'Baikova', age: 18}, 
-            {id: 2, name: 'Pasha', surname: 'Sukhov',  age: 16}
-        ];
-    @observable edit = false;
-    deleteUser(id: number) {
-        this.edit = false;
-        this.arr = this.arr.filter(el=> el.id !== id);
-    }
-    editUser(user: User) {  
-        this.arr = this.arr.map((el => el.id===user.id ? el=user : el));
-        this.edit = false;
-    }
-    addUser(user: User) {   
-        if (!user.name || !user.surname) 
-            return;
-        this.arr.push(user);
-    };
+  @observable arr = [
+    { id: 1, name: "Tanya", surname: "Baikova", age: 18 },
+    { id: 2, name: "Pasha", surname: "Sukhov", age: 16 }
+  ];
+
+  @observable userToEdit?: User;
+
+  deleteUser(id: number) {
+    this.arr = this.arr.filter(el => el.id !== id);
+  }
+
+  setUserToEdit(user: User) {
+    console.log({ ...user });
+
+    this.userToEdit = user;
+  }
+
+  editUser(user: User) {
+    this.arr = this.arr.map(el => (el.id === user.id ? (el = user) : el));
+    this.userToEdit = undefined;
+  }
+
+  addUser(user: User) {
+    if (!user.name || !user.surname) return;
+    this.arr.push(user);
+  }
 }
 
 const users = new Users();
