d15631132207e7b2066242d9f614dd2f24003023
Arseny Kruglikov <Kruglikov.A.D@sberbank.ru>
2020-03-31 19:50:26 +0300

    fix callbacks

--- src/App.tsx
+++ src/App.tsx
@@ -52,55 +52,73 @@ const AddEditUser: FC<Partial<User> & { isEdit?: boolean; saveUser(user: User): 
   );
 };
 
-const ViewUsers: FC<{ user: User; setUserToEdit(user: User): void; deleteUser(user: User): void }> = 
-observer(({ user, setUserToEdit, deleteUser }) => {
-  const handleDelete = useCallback(()=>{
-    deleteUser(user);
-  },[])
-  const handleEdit = useCallback(() => {
-    setUserToEdit(user);
-  }, []);
-  return (
-    <>
-      <div className="row">
-        <div className="col">{user.id}</div>
-        <div className="col">{user.name}</div>
-        <div className="col">{user.surname}</div>
-        <div className="col">{user.age}</div>
-        <button onClick={handleDelete}>Delete</button>
-        <button onClick={handleEdit}>Edit</button>
-      </div>
-    </>
-  );
-});
+const ViewUsers: FC<{ user: User; setUserToEdit(user: User): void; deleteUser(user: User): void }> = observer(
+  ({ user, setUserToEdit, deleteUser }) => {
+    const handleDelete = useCallback(() => {
+      deleteUser(user);
+    }, [deleteUser, user]);
 
-const UsersList: FC<{usersList: User[]}>=observer(({usersList}) =>{
-  return (<>
-    <h2>View Users</h2>
-    {usersList.map(user => (
-      <ViewUsers key={user.id} 
-                 user={user} 
-                 setUserToEdit={(user)=>users.setUserToEdit(user)} 
-                 deleteUser={(user)=>users.deleteUser(user)} />
-        ))
-    }
-    </>
-  )
-})
+    const handleEdit = useCallback(() => {
+      setUserToEdit(user);
+    }, [setUserToEdit, user]);
+
+    return (
+      <>
+        <div className="row">
+          <div className="col">{user.id}</div>
+          <div className="col">{user.name}</div>
+          <div className="col">{user.surname}</div>
+          <div className="col">{user.age}</div>
+          <button onClick={handleDelete}>Delete</button>
+          <button onClick={handleEdit}>Edit</button>
+        </div>
+      </>
+    );
+  }
+);
+
+const UsersList: FC<{ usersList: User[]; setUserToEdit(user: User): void; deleteUser(user: User): void }> = observer(
+  ({ usersList, deleteUser, setUserToEdit }) => {
+    return (
+      <>
+        <h2>View Users</h2>
+        {usersList.map(user => (
+          <ViewUsers key={user.id} user={user} setUserToEdit={setUserToEdit} deleteUser={deleteUser} />
+        ))}
+      </>
+    );
+  }
+);
 
 const App = observer(() => {
+  const addUser = useCallback((user: User) => {
+    users.addUser(user);
+  }, []);
+
+  const editUser = useCallback((user: User) => {
+    users.editUser(user);
+  }, []);
+
+  const deleteUser = useCallback((user: User) => {
+    users.deleteUser(user);
+  }, []);
+
+  const setUserToEdit = useCallback((user: User) => {
+    users.setUserToEdit(user);
+  }, []);
+
   return (
     <>
       <div>
         <h2>AddUser</h2>
-        <AddEditUser saveUser={user => users.addUser(user)} />
+        <AddEditUser saveUser={addUser} />
       </div>
       {users.userToEdit ? (
         <div>
           <h2>Edit user</h2>
           <AddEditUser
             isEdit
-            saveUser={user => users.editUser(user)}
+            saveUser={editUser}
             name={users.userToEdit.name}
             surname={users.userToEdit.surname}
             age={users.userToEdit.age}
@@ -109,7 +127,7 @@ const App = observer(() => {
         </div>
       ) : null}
       <div>
-        <UsersList usersList={users.arr}/>
+        <UsersList deleteUser={deleteUser} setUserToEdit={setUserToEdit} usersList={users.arr} />
       </div>
     </>
   );
